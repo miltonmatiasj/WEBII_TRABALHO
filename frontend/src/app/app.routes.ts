168cda083de7a005fc8c2aa7d53f650f479bfb09
eduardoparolin <eduparolin@gmail.com>
2025-03-27 10:02:42 -0300

    chore: added a test route (/tests) to validate material components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {TestComponentComponent} from "./test-component/test-component.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tests',
+    component: TestComponentComponent,
+    canActivate: [],
+  }
+];
