35b3f230e46f58b100e29160cbf9dd9f427e48ea
Tiago Salles <teeisvee@hotmail.com>
2025-04-01 22:51:07 -0300

    Feat (RF05): Budget Component
    Created a component to the budget screen from client side

---
+++ frontend/src/app/show-budget-component/show-budget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-budget',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-budget.component.html',
+  styleUrl: './show-budget.component.scss'
+})
+export class ShowBudgetComponent {
+
+}
