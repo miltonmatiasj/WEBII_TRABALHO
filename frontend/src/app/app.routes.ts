939527bf04bf0814689611bde498258f41f9b678
Felipe Bassetti <bassettifelipe@gmail.com>
2025-04-04 15:12:13 -0300

    feat(RF010) : created the payment component and added in the routes path's.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+<<<<<<< HEAD
 import { CategoryComponent } from './category/category.component';
 import { MaintenanceRequestDetailsComponent } from './maintenance-request-details/maintenance-request-details.component';
+=======
+import { PaymentComponent } from './payment/payment.component';
+>>>>>>> 8024240 (feat(RF010) : created the payment component and added in the routes path's.)
 
 export const routes: Routes = [
   {
@@ -20,5 +24,10 @@ export const routes: Routes = [
   {
     path: 'maintenance-request-details/:id',
     component: MaintenanceRequestDetailsComponent,
+  },
+  {
+    path: 'payment',
+    //component: PaymentComponent,
+    loadComponent: () => import('./payment/payment.component').then(m => m.PaymentComponent)
   },
 ];
