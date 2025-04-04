6ede8e0f926aaa2de7bac17a36b542d66fd923cd
Viniciusabprr <risktds@gmail.com>
2025-04-04 13:22:57 -0300

    feat(solicitacao-manutencao): inserir protótipo HTML e CSS da tela

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistroComponent } from './features/registro/interfaces/registro/registro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
