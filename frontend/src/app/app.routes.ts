802424081708b0b00c825e1d1c8700139191c486
Felipe Bassetti <bassettifelipe@gmail.com>
2025-04-04 15:12:13 -0300

    feat(RF010) : created the payment component and added in the routes path's.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { AuthGuard } from './authentication/auth.guard';
 import { LoginComponent } from './authentication/login/login.component';
+import { PaymentComponent } from './payment/payment.component';
 
 export const routes: Routes = [
   {
@@ -17,5 +18,10 @@ export const routes: Routes = [
     path: 'tests',
     component: TestComponentComponent,
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'payment',
+    //component: PaymentComponent,
+    loadComponent: () => import('./payment/payment.component').then(m => m.PaymentComponent)
   },
 ];
