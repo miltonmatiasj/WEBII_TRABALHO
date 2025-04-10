87565a8841aa72f6f8d9ec0fe95d8e39bbd16cef
Milton Matias Júnior <miltonmatiasj@gmail.com>
2025-04-10 14:44:12 -0300

    fix(RF008): remove non existent import from app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { TestComponentComponent } from './test-component/test-component.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { MaintenanceRequestDetailsComponent } from './maintenance-request-details/maintenance-request-details.component';
