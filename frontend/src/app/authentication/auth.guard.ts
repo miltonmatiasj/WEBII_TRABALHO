c8cf2956fc338e6cc29e40d77c9aec8faae1439f
Milton Matias Junior <milton.matias@wises.com.br>
2025-04-23 19:38:00 -0300

    fix: fix typing error

--- frontend/src/app/authentication/auth.guard.ts
+++ frontend/src/app/authentication/auth.guard.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MockAuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const currentUser = localStorage.getItem('currentUser');
-    
+    const currentUser = localStorage.getItem('CurrentUser');
+
     if (currentUser) {
       return true;
     }
