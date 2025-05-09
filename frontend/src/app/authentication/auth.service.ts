1d9da0627a7ef668afbfdf96101689db0f11b705
eduardoparolin <eduparolin@gmail.com>
2025-05-09 16:57:24 -0300

    feat: implement login functionality using HTTP request and update base URL in environment

--- frontend/src/app/authentication/auth.service.ts
+++ frontend/src/app/authentication/auth.service.ts
@@ -3,6 +3,11 @@ import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {AuthMockedService} from './auth-mocked.service';
 import {User} from "../User";
+import {lastValueFrom} from "rxjs";
+
+export type LoginResponse = {
+
+}
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +18,6 @@ export class AuthService {
   currentUser = signal<User | null>(null)
   http = inject(HttpClient)
   router = inject(Router)
-  authMockedService = inject(AuthMockedService)
 
   constructor() {
     const savedToken = localStorage.getItem('token');
@@ -22,30 +26,19 @@ export class AuthService {
     this.email.set(savedEmail);
     this.currentUser.set(User.fromLocalStorage());
   }
-
   async login(email: string, password: string) {
-    this.email.set(email);
-    const user = this.authMockedService.findUserByEmail(email);
-
-    if (user && user.comparePassword(password)) {
-      const mockToken = 'mock-token-' + Math.random().toString(36).substring(2);
-      this.token.set(mockToken);
-      localStorage.setItem('token', mockToken);
-      localStorage.setItem('email', email);
-      this.currentUser.set(user);
-      localStorage.setItem('currentUser', JSON.stringify(user));
-
-      return true;
+    const loginResult = await lastValueFrom(this.http.post<LoginResponse>('/auth/login', {email, password}))
+      .catch(() => {
+        console.log('Erro ao fazer login.');
+        return null;
+      });
+    if (loginResult == null) {
+      return;
     }
-    return false;
+
   }
 
   async logout() {
-    this.token.set(null);
-    this.email.set(null);
-    this.currentUser.set(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('email');
-    await this.router.navigate(['/login']);
+
   }
 }
