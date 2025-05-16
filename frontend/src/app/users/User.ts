ae6040a177a057353aec8aed13519e4f6698cd73
eduardoparolin <eduparolin@gmail.com>
2025-05-16 19:29:45 -0300

    feat: update EmployeeController endpoint and refactor Address type to class with constructors and static methods

--- frontend/src/app/users/User.ts
+++ frontend/src/app/users/User.ts
@@ -1,6 +1,6 @@
 const mockAdminEmails = ['eduparolin+admin@gmail.com', 'testeadmin@gmail.com'];
 
-export type Address = {
+export class Address {
   zipCode: string;
   street: string;
   neighborhood: string;
@@ -8,7 +8,41 @@ export type Address = {
   state: string;
   number: string;
   complement?: string;
-};
+
+  constructor(
+    zipCode: string,
+    street: string,
+    neighborhood: string,
+    city: string,
+    state: string,
+    number: string,
+    complement?: string
+  ) {
+    this.zipCode = zipCode;
+    this.street = street;
+    this.neighborhood = neighborhood;
+    this.city = city;
+    this.state = state;
+    this.number = number;
+    this.complement = complement;
+  }
+
+  static fromJson(json: { [key: string]: any }): Address {
+    return new Address(
+      json['zipCode'],
+      json['street'],
+      json['neighborhood'],
+      json['city'],
+      json['state'],
+      json['number'],
+      json['complement']
+    );
+  }
+
+  static empty(): Address {
+    return new Address('', '', '', '', '', '', '');
+  }
+}
 
 function generateRandomString(length: number = 30): string {
   const characters =
